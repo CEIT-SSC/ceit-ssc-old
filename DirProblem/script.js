3efe8c3a1a57c486b14bafedd4f5e2e4e982ec31
9831065 <najafizadeh21@gmail.com>
2021-03-06 19:19:08 +0330

    Problem fixed

--- DirProblem/script.js
+++ DirProblem/script.js
@@ -1,13 +1,17 @@
 $(document).ready(function () {
+	
     $("html").children().css("direction","rtl");
 	
-	$(".change-lang").on("click", function () {
+	$(".change-lang").on("click", function () 
+	{
 		if( $("body").css("direction") == "rtl" )
 		{
 			$("html").children().css("direction","ltr");
+			$("div>a").css("float","left");
 		} else 
 		{
 			$("html").children().css("direction","rtl");
+			$("div>a").css("float","right");
 		}
 	});
 });
